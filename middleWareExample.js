ca5cb7b60e36f9d583a87aba25346ec0a958a546
Jessica Fisk <fisk.jess@gmail.com>
2023-03-06 11:46:39 +0000

    remove examople router

--- middleWareExample.js
+++ middleWareExample.js
@@ -13,7 +13,7 @@ const middleOne = async (req, res, next) => {
     next();
 };
 // "next()"- express is expecting a third (optional) argument called next,
-// this will pass the request onto thr next function once stuff has happened.
+// this will pass the request onto the next function once stuff has happened.
 
 const middleTwo = async(req, res, next) => {
     console.log ("start middleTwo", req.body)
@@ -21,13 +21,11 @@ const middleTwo = async(req, res, next) => {
     next();
 }
 // request come to url"/example"
-// request paases to middleIne
+// request passes to middleOne
 // things happen in middleOne
 // The "next()" function is called and passes the request to middleTwo
 // Things happen in middleTwo
 // "next()" passes request to finalFunc
-
-
 
 exampleRouter.post("/example", middleOne, middleTwo, finalFunc)
 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config()
 const express = require("express")
 
-const exampleRouter = require("../middleWareExample");
+
 
 const port = process.env.PORT || 5001;
 
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use (exampleRouter);
+
 
 app.get("/health", (req, res) =>
 res.status(200).json ({ message: "API is working" }))
