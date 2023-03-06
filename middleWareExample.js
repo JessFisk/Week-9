a99bb2271385f3b4dc5dc262b60b0a30b335a24d
Jessica Fisk <fisk.jess@gmail.com>
2023-03-06 11:19:04 +0000

    added notes

--- middleWareExample.js
+++ middleWareExample.js
@@ -12,12 +12,22 @@ const middleOne = async (req, res, next) => {
     req.body["middleOne"] = "Im from the middleOne func";
     next();
 };
+// "next()"- express is expecting a third (optional) argument called next,
+// this will pass the request onto thr next function once stuff has happened.
 
 const middleTwo = async(req, res, next) => {
     console.log ("start middleTwo", req.body)
     req.body ["middleTwo"] = "Im from middleTwo" ;
     next();
 }
+// request come to url"/example"
+// request paases to middleIne
+// things happen in middleOne
+// The "next()" function is called and passes the request to middleTwo
+// Things happen in middleTwo
+// "next()" passes request to finalFunc
+
+
 
 exampleRouter.post("/example", middleOne, middleTwo, finalFunc)
 
