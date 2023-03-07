f26cb5fcd53b85f30f20a6f55d4440b847b403ba
Jessica Fisk <fisk.jess@gmail.com>
2023-03-07 09:18:59 +0000

    added routes/mondays work

--- src/server.js
+++ src/server.js
@@ -1,19 +1,26 @@
 require("dotenv").config()
 const express = require("express")
 
-
-
 const port = process.env.PORT || 5001;
+
+const userRouter = require("./users/routes");
+
+const User = require("./users/model");
 
 const app = express();
 
 app.use(express.json());
 
+const syncTables = () => {
+    User.sync({ alter: true, force: false });
+}
 
+app.use(userRouter);
 
 app.get("/health", (req, res) =>
-res.status(200).json ({ message: "API is working" }))
+    res.status(200).json({ message: "API is working" }));
 
 app.listen(port, () => {
-    console.log(`server is listening on port ${port}`)
+    syncTables();
+    console.log(`server is listening on port ${port}`);
 })
